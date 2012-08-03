0b4698e5ba5b5c764d859ac86b7451fc35767c43
cleitonmoura18 <cleitonmoura18@ace5a2d9-4f0c-4642-8350-73b2d22c680b>
2012-08-03 14:08:55 +0000

    Tirando alterações do branch e colocando no trunk
    git-svn-id: https://xp-dev.com/svn/usabilidade@154 ace5a2d9-4f0c-4642-8350-73b2d22c680b

--- Usabilidade/src/main/java/br/ufpi/util/Paginacao.java
+++ Usabilidade/src/main/java/br/ufpi/util/Paginacao.java
@@ -2,6 +2,16 @@ package br.ufpi.util;
 
 import java.util.List;
 
+import br.com.caelum.vraptor.Result;
+
+/**
+ * Usado para fazer paginação de objetos
+ * 
+ * @author Cleiton
+ * 
+ * @param <T>
+ *            Tipo de objeto que vai ser recebida pela classe
+ */
 public class Paginacao<T> {
 	private Long count;
 	private List<T> listObjects;
@@ -30,6 +40,26 @@ public class Paginacao<T> {
 
 	public void setCount(Long count) {
 		this.count = count;
+	}
+
+	/**
+	 * Usado para gerar a paginação de paginas
+	 * 
+	 * @param numeroPagina
+	 *            Informa o numero da página que o usuario esta olhando
+	 * @param qtdObjetosPorPaginas
+	 *            Informa a quantidade de objetos que sera mostrado por paginas
+	 * @param qttObjetosNoBanco
+	 *            Informa a quatindade de objetos que possui na base de Dados
+	 */
+	public void geraPaginacao(int numeroPagina, int qtdObjetosPorPaginas,
+			Long qttObjetosNoBanco, Result result) {
+		int qtdPaginas = (int) (qttObjetosNoBanco / qtdObjetosPorPaginas);
+		if (qttObjetosNoBanco % qtdObjetosPorPaginas != 0) {
+			qtdPaginas++;
+		}
+		result.include("paginaAtual", numeroPagina);
+		result.include("qtdPaginas", qtdPaginas);
 	}
 
 }
