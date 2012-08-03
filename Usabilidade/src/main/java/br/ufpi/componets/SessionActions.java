275767498e87e74b6a2a529657b9437b2ece2958
cleitonmoura18 <cleitonmoura18@ace5a2d9-4f0c-4642-8350-73b2d22c680b>
2012-08-03 14:00:24 +0000

    Tirando alterações do branch e colocando no trunk
    git-svn-id: https://xp-dev.com/svn/usabilidade@149 ace5a2d9-4f0c-4642-8350-73b2d22c680b

--- Usabilidade/src/main/java/br/ufpi/componets/SessionActions.java
+++ Usabilidade/src/main/java/br/ufpi/componets/SessionActions.java
@@ -7,11 +7,11 @@ import javax.annotation.PreDestroy;
 
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.SessionScoped;
-import br.ufpi.models.Acao;
+import br.ufpi.models.Action;
 
 /**
- * Usado para gravação do fluxo. Quando terminar a gravação do fluxo deleta a
- * seção.
+ * Usado para gravaÃ§Ã£o do fluxo. Quando terminar a gravaÃ§Ã£o do fluxo deleta a
+ * seÃ§Ã£o.
  * 
  * @author Cleiton
  * 
@@ -20,9 +20,9 @@ import br.ufpi.models.Acao;
 @Component
 public class SessionActions {
 	/**
-	 * Lista de ações de um determinado FluxoComponente
+	 * Lista de aÃ§Ãµes de um determinado FluxoComponente
 	 */
-	private List<Acao> acoes;
+	private List<Action> acoes;
 	/**
 	 * False se nao for a primeira vez que a pagina foi aberta True se for a
 	 * primeira vez.
@@ -47,11 +47,11 @@ public class SessionActions {
 		acoes = null;
 	}
 
-	public List<Acao> getAcoes() {
+	public List<Action> getAcoes() {
 		return acoes;
 	}
 
-	public void setAcoes(List<Acao> acoes) {
+	public void setAcoes(List<Action> acoes) {
 		this.acoes = acoes;
 	}
 
@@ -59,9 +59,9 @@ public class SessionActions {
 		primeiraPagina = true;
 	}
 
-	public boolean addAction(List<Acao> actions) {
+	public boolean addAction(List<Action> actions) {
 		if (acoes == null)
-			this.acoes = new ArrayList<Acao>();
+			this.acoes = new ArrayList<Action>();
 		return this.acoes.addAll(actions);
 	}
 
