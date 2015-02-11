6ca7579839628d0961cd4c7413f6bfd2deb5d505
matheusmmcs <matheusmmcs@ace5a2d9-4f0c-4642-8350-73b2d22c680b>
2015-02-11 02:54:02 +0000

    Alteracao na configuracao de banco, permitindo salvar a senha do banco de producao
    git-svn-id: https://xp-dev.com/svn/usabilidade@318 ace5a2d9-4f0c-4642-8350-73b2d22c680b

--- Usabilidade/useskill-capture/models/index.js
+++ Usabilidade/useskill-capture/models/index.js
@@ -2,28 +2,28 @@ var fs        = require('fs')
   , path      = require('path')
   , Sequelize = require('sequelize')
   , lodash    = require('lodash')
-  , sequelize = new Sequelize('usabilidade_onthefly', 'root', '', {
+  //, sequelize = new Sequelize('usabilidade_datamining', 'root', '', {
+  , sequelize = new Sequelize('usabilidade_datamining', 'root', 'gogokillers2013', {
       logging: false
   })
-  , db        = {}
+  , db        = {};
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(function(file) {
     return ((file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) == '.js'))
   })
   .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
-  })
+  });
 
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].options.hasOwnProperty('associate')) {
     db[modelName].options.associate(db)
   }
-})
+});
 
 module.exports = lodash.extend({
   sequelize: sequelize,
   Sequelize: Sequelize
-}, db)
+}, db);
